0a29d0f4905a2e07a1e83478e5b0463358b8abaa
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-03-05 17:48:06 +0400

    Adding new files

---
+++ Desktop/cpp/day2/4_calculator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+  float num1, num2;
+  char op;
+  cout << "Please enter two numbers to start calculation." << "\n";
+  cout << "First number:", cin >> num1, "\n";
+  cout << "Operator:", cin >> op, "\n";
+  cout << "Second number:", cin >> num2, "\n";
+  cout << "Result:";
+  switch (op) {
+    case '+' :
+      cout << num1 + num2;
+      break;
+    case '-' :
+      cout << num1 - num2;
+      break;
+    case '*' :
+      cout << num1 * num2;
+      break;
+    case '/' :
+      cout << num1 / num2;
+      break;
+    default :
+      cout << "This operator isn't defined.Please use +,-,*,/";
+  return 0;
+  }
+}
