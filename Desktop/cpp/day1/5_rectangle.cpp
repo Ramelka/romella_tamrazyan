aba391ee450357b2c62ad52ccad7e9ee3917aa26
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-03-05 14:29:14 +0400

    Adding new lines

--- Desktop/cpp/day1/5_rectangle.cpp
+++ Desktop/cpp/day1/5_rectangle.cpp
@@ -8,15 +8,20 @@ int main () {
   
     //Answering the sides of rectangle.
     cout << "What are the sides of a rectangle?" << endl;
+
     cin >> a >> b;
     
     system("clear");
     
     //Counting the perimeter(P) and area(S).
     cout << "a = " << a << "," << "b = " << b << endl;
+
     int P = 2 * (a + b);
+
     cout << "P = " << P << endl;
+
     int S = a * b;
+
     cout << "S = " << S;
     
     return 0;
---
+++ Desktop/cpp/day2/1_week.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int weeknum;
+    
+    cout << "Please write a number of the week and I'll say the name of that day."
+    cin >> weeknum;
+    
+    switch (weeknum) {
+        case 1 :
+	   cout << "The first day of week is Monday.";
+	   break;
+	case 2 :
+	   cout << "The second day of week is Tuesday.";
+           break;
+	case 3 :
+           cout << "The third day of week is Wednesday.";
+           break;
+	case 4 :
+           cout << "The fourth day of week is Thursday.";
+           break;
+	case 5 :
+           cout << "The fifth day of week is Friday.";
+           break;
+	case 6 :
+           cout << "The sixth day of week is Saturday.";
+           break;
+	case 7 :
+           cout << "The seventh day of week is Sunday.";
+           break;
+    }
+    return 0;	    
+}
