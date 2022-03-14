012498866d2ea12a0df4eafc7f6e10b37ccfa6c3
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-03-15 01:30:00 +0400

    Correcting mistakes and upgrading the file

--- cpp/day4/6_palindrom.cpp
+++ cpp/day4/6_palindrom.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
-#include <cstring>
+#include <string.h>
 
 using namespace std;
 
 int main() {
-  bool b1 = true;
-  char word[20];
+  bool isPalindrome = true;
+  char word[256];
+
   cout << "Enter a word to check it." << endl;
-  cin >> word[20];
- 
+  cin >> word;
+  
   int length = strlen(word);
  
-  for (int i = 0; i < length; i++) {
-   if (word[i] == word[length - i - 1]) 
-    b1 = true;
-    break;
+  for (int i = 0; i < length / 2; i++) { 
+    if (word[i] != word[length - i - 1]) {
+      isPalindrome = false;
+      break;
+   }
  }
  
-  if (b1)  cout << "Polindrom word" << endl;
-  else     cout << "Not polindrom" << endl;
+  if (isPalindrome)  
+    cout << "Palindrome word" << endl;
+  else       
+    cout << "Not palindrome word" << endl;
   
   return 0;
 }
