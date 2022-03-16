7048b80904b60dc446b9256405222ef978046e0a
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-03-16 12:47:44 +0400

    Creating my own atoi function

--- cpp/day8/atoi.cpp
+++ cpp/day8/atoi.cpp
@@ -1,13 +1,20 @@
 #include "atoi.h"
-#include <iostream>
-
-using namespace std;
 
 int own_atoi(char* x) {
   int result = 0;
+	
+  int j = 0;
 
-  for (int i = 0; x[i] != '\0'; ++i) {
-    result = result * 10 + x[i] - '\0';
+  if (x[0] == '-') {
+    j = 1;
+  }
+
+  for (int i = j; x[i] != '\0'; i++) {
+    result = result * 10 + x[i] - '0';
+  }
+
+  if (x[0] == '-') {
+    return -result;
   }
 
   return result;
