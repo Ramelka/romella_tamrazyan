9b715499d1f0c3417665ea1dbe4b4ccb2aa59218
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-03-16 12:48:22 +0400

    Using my own atoi function in main function

--- cpp/day8/atoifunct.cpp
+++ cpp/day8/atoifunct.cpp
@@ -1,5 +1,5 @@
-#include "atoi.h"
 #include <iostream>
+#include "atoi.h"
 
 using namespace std;
 
@@ -7,10 +7,10 @@ int main(int argc, char* argv[]) {
   int sum1 = 0;
   int sum2 = 0;
 
-  for (int i = 0; i < argc; i++) {
+  for (int i = 1; i < argc; i++) {
     sum1 += atoi(argv[i]);
   }
-  for (int i = 0; i < argc; i++) {
+  for (int i = 1; i < argc; i++) {
     sum2 += own_atoi(argv[i]);
   }
   cout << "Atoi = " << sum1 << endl << "My atoi = " << sum2 << endl;
