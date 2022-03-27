8334c672de41853eb23a35837dee32f41a94934b
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-03-27 19:23:00 +0400

    Creating a program to check the spelling of English words

--- cpp/day12/spelling.cpp
+++ cpp/day12/spelling.cpp
@@ -4,41 +4,58 @@
 
 using namespace std;
 
+string check(string);
+
 int main() {
   string text; 
   cout << "Please enter a text to check it." << endl;
   getline(cin, text);
+  text += " ";
   
-  string filename = "words.txt";
-  ifstream word;
-  word.open(filename);
-  string line;
-  int incorrect = 0; 
+  ofstream f1;
+  f1.open("corrected.txt");
   
-  ofstream corr;
-  corr.open("corrected.txt");
-
-  try {
-    if (!word) {
-      throw (filename);
-    }
-    while (word.is_open()) {
-      getline(word, line);
-        for (int i = 0; text[i] != '\0';i++) {
-	  if (text != line) {
-	    incorrect = 1;
-	    break; 
-	  } 	  
-         
-	}
-    }
-     word.close();
-     corr.close();
-    if (incorrect != 1) {
-    }
-  }
-  catch (string s) {
-    cout << "File " << s << " isn't opened" << endl;
-  }
+  string split;
+  int i = 0;
+  while (text[i] != '\0') {
+      split = "" ;
+      for (; text[i] != ' '; i++) {
+          split += text[i];
+      }
+      i++;
+    //cout << check(split) << endl;
+      f1 << check(split) << endl;
+  } 
+  f1.close();
   return 0;
 }
+
+string check(string correct) {
+
+  ifstream f;
+  f.open("words.txt");
+
+  string words;
+  int count = 0;
+  while (!f.eof()) {
+      count = 0;
+      getline(f, words);
+      for (int i = 0; correct[i] != '\0'; i++) {
+	  if (correct.length() == words.length()) {
+              if (correct[i] != words[i]) {
+	          count++;
+	      }
+
+	  }
+      }
+      if (count == 1)  {
+          break;
+      }	  
+  }
+  f.close();
+  if (count != 1) {
+      return correct;
+  }
+  return words;
+}
+
