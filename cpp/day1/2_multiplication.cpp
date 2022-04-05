b6b6829a395e3b9fd9c3749f1e6eb410f66a01a8
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-04-05 09:32:26 +0400

    Changing the variable name and correcting the coding style

--- cpp/day1/2_multiplication.cpp
+++ cpp/day1/2_multiplication.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
+
 using namespace std;
 
 int main () {
 
-    int num;
-    int m = 0;
-    
-    cout << "Please enter a number." << endl;
-    cin >> num;
-    
-    while (m < 10) {
-        int sum = num * m;
-        cout << num << " x " << m << " = " << sum << endl;
-        m++;
-    }
-    
-    
-    return 0;
+  int num1;
+  int num2 = 0;
+  int sum = 0;  
+  
+  cout << "Please enter a number." << endl;
+  cin >> num1;
+  cout << endl << "			Multiplication table of " << num1 << endl;  
+  
+  while (num2 < 10) {
+      sum = num1 * num2;
+      cout << "				" << num1 << " x " << num2 << " = " << sum << endl;
+      num2++;
+  }
+  
+  return 0;
 }
