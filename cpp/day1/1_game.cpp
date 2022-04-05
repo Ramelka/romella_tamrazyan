b1531f111c07a46ef34e0cd61de3fdb0c1cae6ed
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-04-05 09:10:20 +0400

    Changing the coding style

--- cpp/day1/1_game.cpp
+++ cpp/day1/1_game.cpp
@@ -5,30 +5,31 @@ using namespace std;
 
 int main () {
 
-    int number, yournumber;
+  int number, yournumber;
 
-    while (!(number >= 1 && number <=100)) {
-        cout << "Please enter a number" << endl;
-        cin >> number;
-    }
-    system ("clear");
+  while (!(number >= 1 && number <=100)) {
+      cout << "Please enter a number" << endl;
+      cin >> number;
+  }
+  
+  system ("clear");
     
-    cout << "Try to guess the number" << endl;
-    cin >> yournumber;
+  cout << "Try to guess the number" << endl;
+  cin >> yournumber;
     
-    do {
-        while (yournumber > number) {
-            cout << "The memorized number is smaller than your number" << endl;
-            cin >> yournumber;
-        }
+  do {
+      while (yournumber > number) {
+          cout << "The memorized number is smaller than your number" << endl;
+          cin >> yournumber;
+      }
         
-        while (yournumber < number) {
-            cout << "The memorized number is bigger than your number" << endl;
-            cin >> yournumber;
-        } 
-    }
-    while (number != yournumber);
-        cout << "Good job! You won!";
+      while (yournumber < number) {
+          cout << "The memorized number is bigger than your number" << endl;
+          cin >> yournumber;
+      } 
+  }
+  while (number != yournumber);
+      cout << "Good job! You won!" << endl;
 
   
   return 0;
