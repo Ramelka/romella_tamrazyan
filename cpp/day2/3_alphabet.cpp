ffe267d9b6dfa7b18aa46c8e486debb17fb080e1
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-04-05 14:13:39 +0400

    Creating a program do determine consonant and vowel letters

--- cpp/day2/3_alphabet.cpp
+++ cpp/day2/3_alphabet.cpp
@@ -7,27 +7,26 @@ int main() {
   char letter;
   
   do {
-    cout << "Write any letter of alphabet to determine its type." << "\n";
-  } while (letter < 'A' || letter > 'z');
-  
-  cin >> letter;
+    cout << "Write any letter of alphabet to determine its type.\n";
+    cin >> letter;
+  } while ((letter < 'A' || letter > 'Z') && (letter < 'a' || letter > 'z'));
   
   switch (letter) {
-    case 'a' :
-    case 'i' :
-    case 'o' :
-    case 'e' :
-    case 'u' :
-    case 'A' :
-    case 'I' :
-    case 'O' :
-    case 'E' :
-    case 'U' :
-      cout << "It's a vowel sound.";
-      break;
-    default :
-      cout << "It's a consonant sound";
-      break;
+      case 'a' :
+      case 'i' :
+      case 'o' :
+      case 'e' :
+      case 'u' :
+      case 'A' :
+      case 'I' :
+      case 'O' :
+      case 'E' :
+      case 'U' :
+          cout << "It's a vowel sound.\n";
+          break;
+      default :
+          cout << "It's a consonant sound.\n";
+          break;
   }
   return 0;
 }
