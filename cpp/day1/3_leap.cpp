35dc50ac02eeeabb3405f181135e34dc50989d0f
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-04-05 09:53:40 +0400

    Changing the style of the program

--- cpp/day1/3_leap.cpp
+++ cpp/day1/3_leap.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
+
 using namespace std;
 
 int main () {
 
-    int year = 2020;
-    int n = 0;
-    
-    while (n < 20) {
-        year = year + 4;
-        cout << year << ",";
-        n++;
-    }
-   
-    return 0;
+  int year = 2022;
+  int quant = 0;
+  
+  cout << "  		       Next 20 leap years from " << year << " are:" << endl;
+  
+  while (quant < 20) {
+      if (year % 4 == 0) {
+          cout << year << ",";
+          quant++;
+      }   
+      year++;
+  }   
+  
+  cout << endl;
+ 
+  return 0;
 }
+
