09b110fdae482b7b76f77b8688e662cea80800ea
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-04-05 22:21:09 +0400

    Creating a program to find maximum from array numbers

--- cpp/day4/3_max.cpp
+++ cpp/day4/3_max.cpp
@@ -5,28 +5,28 @@
 using namespace std;
 
 int main() {
-  int n;
+  int num;
   do {
     cout << "How many numbers do you want to generate?" << endl;
-    cin >> n;
-  } while (n > 100);
+    cin >> num;
+  } while (num > 100);
   
   int array[100];
   srand(time(0));
   
-  for (int i = 0; i < n; i++) {
-    array[i] = rand()%1000 + 1;
-    cout << array[i] << ",";
+  for (int i = 0; i < num; i++) {
+      array[i] = rand() % 100 + 1;
+      cout << array[i] << ", ";
   }
-    cout << endl;
+  cout << endl;
     
-    int max = array[0];
+  int max = array[0];
   
-  for (int i = 1; i < n; i++) {
-    if (max < array[i])
-      max = array[i];
+  for (int i = 1; i < num; i++) {
+      if (max < array[i])
+          max = array[i];
   }
-    cout <<  "Max" << "=" << max << endl;
+  cout <<  "Max" << " = " << max << endl;
   return 0;
 }
 
