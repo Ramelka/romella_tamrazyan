2a436729b8917133baa4f6d95091b2f00162b250
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-04-05 13:48:16 +0400

    Creating a program for weekdays

--- cpp/day2/1_week.cpp
+++ cpp/day2/1_week.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
@@ -9,31 +10,30 @@ int main() {
   cin >> weeknum;
     
   switch (weeknum) {
-    case 1 :
-      cout << "The first day of week is Monday.";
-      break;
-    case 2 :
-      cout << "The second day of week is Tuesday.";
-      break;
-    case 3 :
-      cout << "The third day of week is Wednesday.";
-      break;
-    case 4 :
-      cout << "The fourth day of week is Thursday.";
-      break;
-    case 5 :
-      cout << "The fifth day of week is Friday.";
-      break;
-    case 6 :
-      cout << "The sixth day of week is Saturday.";
-      break;
-    case 7 :
-      cout << "The seventh day of week is Sunday.";
-      break;
-    default :
-      cout << "A week has only 7 days.";
-      break;
+      case 1 :
+          cout << "The first day of week is Monday.\n";
+          break;
+      case 2 :
+          cout << "The second day of week is Tuesday.\n";
+          break;
+      case 3 :
+          cout << "The third day of week is Wednesday.\n";
+          break;
+      case 4 :
+          cout << "The fourth day of week is Thursday.\n";
+          break;
+      case 5 :
+          cout << "The fifth day of week is Friday.\n";
+          break;
+      case 6 :
+          cout << "The sixth day of week is Saturday.\n";
+          break;
+      case 7 :
+          cout << "The seventh day of week is Sunday.\n";
+          break;
+      default :
+          cout << "A week has only 7 days.\n";
+          break;
   }
-    return 0;	    
+  return 0;	    
 }
-
