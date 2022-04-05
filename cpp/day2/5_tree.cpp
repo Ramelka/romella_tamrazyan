e3c8ee9b0f7c4bd6626ce285288559340b245095
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-04-05 15:29:35 +0400

    Changing the version of program using for loop instead of while

--- cpp/day2/5_tree.cpp
+++ cpp/day2/5_tree.cpp
@@ -3,72 +3,55 @@
 using namespace std;
 
 int main() {
-  
-  int n;
-  
-  do {
-      cout << "Enter a positive number" << "\n";
-      cin >> n;
-      
-  } while (n < 0);
+  int size;
+  cout << "Enter a size for tree" << endl;
+  cin >> size;
 
   //Number 1
-  int tmp = n;
-  
-  while (tmp) {
-    int i = tmp;
-    while (i--) {
-      cout << "*";
-    }
+  for (int i = 0; i < size; i++) {
+      for (int j = 0; j <= i; j++) {
+          cout << "*";
+      }
       cout << endl;
-      tmp--;
   }
+
   cout << endl;
-  
+
   //Number 2
-  tmp = n;
-  
-  while (tmp) {
-    int i = tmp - 1;
-    while (i--) {
-      cout << " ";
-    }
-    i = n - tmp + 1;
-    while (i--) {
-      cout << "*";
-    }
-    cout << endl;
-    tmp--;
-  }  
-  cout << endl;
-  
-  //Number 3
-  tmp = n;
-  while (tmp) {
-    int i = n - tmp + 1;
-    while (i--) {
-      cout << "*";
-    }
-    cout << endl;
-    tmp--;
+  for (int i = 0; i < size; i++) {
+      for (int j = i; j < size; j++) {
+          cout << "*";
+      }
+      cout << endl;
   }
+
   cout << endl;
-  
+
+  //Number 3
+  for (int i = 0; i < size; i++) {
+      for (int j = i; j < size - 1; j++) {
+          cout << " ";
+      }
+      for (int k = 0; k <= i; k++) {
+          cout << "*";
+      }
+      cout << endl;
+  }
+
+  cout << endl;
+
   //Number 4
-  tmp = n;
-  
-  while (tmp) {
-    int i = tmp - 1;
-    while (i--) {
-      cout << " ";
-    }
-  
-    i = n - tmp + 1;
-    while (i--) {
-    cout << "* ";
-    }
-    cout << endl;
-    tmp--;
-  } 
+  for (int i = 0; i < size; i++) {
+      for (int j = i; j < size - 1; j++) {
+          cout << " ";
+      }
+      for (int k = 0; k < 2 * i - 1; k++) {
+          cout << "*";
+      }
+      cout << endl;
+  }
+
+  cout << endl;
+
   return 0;
 }
