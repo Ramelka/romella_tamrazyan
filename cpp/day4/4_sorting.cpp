1c398abc7365b53651ae9637335b684475cbb96d
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-04-05 22:42:06 +0400

    Creating a program to sort array numbers from min to max

--- cpp/day4/4_sorting.cpp
+++ cpp/day4/4_sorting.cpp
@@ -5,39 +5,39 @@
 using namespace std;
 
 int main() {
-  int n, i, j, x;
+  int quant = 0, x = 0;
   do {
-    cout << "How many numbers do you want to generate?" << endl;
-    cin >> n;
-  } while (n > 100);
+      cout << "How many numbers do you want to generate?" << endl;
+      cin >> quant;
+  } while (quant > 100);
 
   int array[100];
   srand(time(0));
 
   cout << "Input: ";
 
-  for (int i = 0; i < n; i++) {
-    array[i] = rand()%100 + 1;
-    cout << array[i] << " ";
+  for (int i = 0; i < quant; i++) {
+      array[i] = rand() % 100 + 1;
+      cout << array[i] << " ";
   }
   
   cout << endl;
 
-  for (i = 0; i < n; i++) {
-
-    for (j = i + 1; j < n; j++) {
-      if (array[i] > array[j]) {
-        x = array[i];
-        array[i] = array[j];
-        array[j] = x;
+  for (int i = 0; i < quant; i++) {
+      for (int j = i + 1; j < quant; j++) {
+          if (array[i] > array[j]) {
+              x = array[i];
+              array[i] = array[j];
+              array[j] = x;
+          } 
       }
-    }
   }
 
   cout << "Output: ";
 
-  for (int i = 0;  i < n; i++) {
-    cout << array[i] << " ";
+  for (int i = 0;  i < quant; i++) {
+      cout << array[i] << " ";
   }
+  cout << endl;
   return 0;
 }
