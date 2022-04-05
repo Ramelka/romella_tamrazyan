71a69f7968d3d606772c3ee11bdcb01d13a3b29d
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-04-05 16:25:58 +0400

    Uploading the program using function

--- cpp/day3/1_game.cpp
+++ cpp/day3/1_game.cpp
@@ -3,37 +3,33 @@
 
 using namespace std;
 
+void compare (int, int);
 int main () {
 
-    int number, yournumber;
+  int number, yournumber;
 
-    while (!(number >= 1 && number <=100)) {
-        cout << "Please enter a number" << endl;
-        cin >> number;
-    }
-    system ("clear");
-    
-    cout << "Try to guess the number" << endl;
-    cin >> yournumber;
-    
-    do {
-        while (yournumber > number) {
-            cout << "The memorized number is smaller than your number" << endl;
-            cin >> yournumber;
-        }
-        
-        while (yournumber < number) {
-            cout << "The memorized number is bigger than your number" << endl;
-            cin >> yournumber;
-        } 
-    }
-    while (number != yournumber);
-        cout << "Good job! You won!";
-
+  while (!(number >= 1 && number <=100)) {
+      cout << "Please enter a number" << endl;
+      cin >> number;
+  }   
   
+  system ("clear");
+    
+  cout << "Try to guess the number" << endl;
+  cin >> yournumber;
+    
+  do {
+      compare (yournumber, number);
+      cin >> yournumber;
+  } while (number != yournumber);
+      cout << "Good job! You won!\n";
+
   return 0;
 }
-
-hgjhgh
-nnbn
-
+void compare (int num1, int num2) {
+  if (num1 > num2) {
+      cout << "The memorized number is smaller than your number\n";
+  } else if (num1 < num2) {
+      cout << "The memorized number is bigger than your number\n";
+  }
+}
