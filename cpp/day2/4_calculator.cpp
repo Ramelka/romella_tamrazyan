5602c37d013663e9914c29693be8f2121331c2d6
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-04-05 14:25:13 +0400

    Creating a program for calculating

--- cpp/day2/4_calculator.cpp
+++ cpp/day2/4_calculator.cpp
@@ -6,29 +6,31 @@ int main() {
   
   float num1, num2;
   char op;
-  cout << "Please enter two numbers to start calculation." << "\n";
-  cout << "First number:", cin >> num1, "\n";
-  cout << "Operator:", cin >> op, "\n";
-  cout << "Second number:", cin >> num2, "\n";
-  cout << "Result:";
+
+  cout << "Please enter two numbers to start calculation.\n";
+  cout << "First number: "; cin >> num1;
+  cout << "Operator: "; cin >> op;
+  cout << "Second number: "; cin >> num2;
+  cout << "Result: ";
+
   switch (op) {
-    case '+' :
-      cout << num1 + num2;
-      break;
-    case '-' :
-      cout << num1 - num2;
-      break;
-    case '*' :
-      cout << num1 * num2;
-      break;
-    case '/' :
-      if (num2 == 0) {
-        cout << "Can't divide by zero.";
-      } break; 
-      cout << num1 / num2;
-      break;
-    default :
-      cout << "This operator isn't defined.Please use +,-,*,/";
+      case '+' :
+          cout << num1 + num2 << endl;
+          break;
+      case '-' :
+          cout << num1 - num2 << endl;
+          break;
+      case '*' :
+          cout << num1 * num2 << endl;
+          break;
+      case '/' :
+          if (num2 == 0) {
+              cout << "Can't divide by zero.\n";
+          } else if (num2 != 0) {
+              cout << num1 / num2 << endl;
+          } break;
+      default :
+          cout << "This operator isn't defined.Please use +,-,*,/ \n";
   }
   return 0;
 }
