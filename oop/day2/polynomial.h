f348b85960b52a681f79c7164367f3f5c013c6c0
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-04-13 02:19:45 +0400

    Creating a header file for polynomial program

---
+++ oop/day2/polynomial.h
@@ -0,0 +1,18 @@
+#ifndef POLYNOMIAL_H
+#define POLYNOMIAL_H
+
+class Polynomial {
+  private:
+      int _degree1, _degree2;
+      int *arr1 = new int[_degree1 + 1];
+      int *arr2 = new int[_degree2 + 1];
+      int *arr3 = new int[10];
+  public:
+      void InputPolynomial();
+      void Add();
+      void Subtract();
+      void Multiply();
+};
+
+
+#endif
