bee021ccb991b013651cce1761598b6ad1872b36
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-04-16 00:13:08 +0400

    Program for executing the polynomial program

--- oop/day2/main.cpp
+++ oop/day2/main.cpp
@@ -1,33 +1,20 @@
 #include "polynomial.h"
-#include <iostream>
 
 using namespace std;
-void Polynomial::InputPolynomial() {
-  cout << "Enter a degree for the first polynomial: ";
-  cin >> _degree1;
-    
-  for (int i = _degree1; i >= 0; i--) {
-      cout << "   Enter a coefficient of x^" << i << ": ";
-      cin >> arr1[i];
-  }
-
-  cout << "Enter a degree for the second polynomial: ";
-  cin >> _degree2;
-    
-  for (int i = _degree2; i >= 0; i--) {
-      cout << "   Enter a coefficient of x^" << i << ": ";
-      cin >> arr2[i];
-  }   
-}
-
+  
 int main() {
 
-  Polynomial x;
-  x.InputPolynomial();
-  x.Add();
-  x.Subtract();
-  x.Multiply();
+  Polynomial first;
+  Polynomial second;
+  Polynomial result;
 
+  first.InputPolynomial();
+  second.InputPolynomial();
+
+  result.Add(first, second);
+  result.Subtract(first, second);
+  result.Multiply(first, second);
+  result.MultiplyByNumber(3, first);
   return 0;
 }
 
