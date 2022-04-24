b65ca0b8046ab705065b8109583d670359899fc0
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-04-24 23:26:44 +0400

    Creating a header file for linked list program

--- data_structures/day1/linked_list.h
+++ data_structures/day1/linked_list.h
@@ -1,6 +1,27 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
+class Node {
+    public:
+        int data;
+        Node *next;
+        Node();
+};
 
+class LinkedList {
+    private:
+        Node *head;
+        Node *tail;
+    public:
+        LinkedList();
+		Node* GetHead(); 
+        void Add(int);              // Add a node at the end of list
+        void AddAt(Node *, int);    // Add a node after the given node 
+        void Remove();              // Remove the last node of list
+        void RemoveAt(Node *);      // Remove node of the given index
+        int GetElement(int);        // Get element of the given node
+        int CountOfNodes();         // Get count of nodes
+        void Print();               // Print the data of nodes
+};
 
 #endif
