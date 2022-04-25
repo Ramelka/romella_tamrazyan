273de20a2e302c8dc250ada9a2ec21cd9fea167f
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-04-26 02:30:22 +0400

    Creating main program for executing the queue program

---
+++ data_structures/day2/queue/main.cpp
@@ -0,0 +1,20 @@
+#include "../day1/linked_list.h"
+#include "queue.h"
+#include <iostream>
+
+int main() {
+
+    Queue q;
+	
+	// Initializing the stack 
+	for (int i = 0; i < 20; i++) {
+        s.PushBack(rand() % 10);
+    }   
+    cout << q;
+    
+	// Remove the first value
+	int value = q.PopFront();
+	cout << q;
+
+    return 0;
+}
