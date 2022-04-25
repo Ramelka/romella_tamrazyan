b803d042313479fac6d3f204de15af2a092c32a8
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-04-26 01:13:21 +0400

    Changing the parameters of functions by indexes

--- data_structures/day1/linked_list.h
+++ data_structures/day1/linked_list.h
@@ -1,27 +1,34 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
+#include <ostream>
+
+using namespace std;
+
 class Node {
     public:
-        int data;
-        Node *next;
+        int _data;
+        Node *_next;
         Node();
 };
 
 class LinkedList {
     private:
-        Node *head;
-        Node *tail;
+        Node *_head;
+        Node *_tail;
+        int _count;
     public:
         LinkedList();
-	Node* GetHead(); 
+        ~LinkedList();
         void Add(int);              // Add a node at the end of list
-        void AddAt(Node *, int);    // Add a node after the given node 
-        void Remove();              // Remove the last node of list
-        void RemoveAt(Node *);      // Remove node of the given index
-        int GetElement(int);        // Get element of the given node
+        void AddAt(int, int);       // Add a node after the given node 
+        void RemoveLast();          // Remove the last node of list
+        void RemoveAt(int);         // Remove node of the given index
+        void RemoveAll();           // Remove all the nodes
+        int GetElementAt(int);      // Get element of the given node
         int CountOfNodes();         // Get count of nodes
-        void Print();               // Print the data of nodes
+        friend ostream& operator << (ostream& COUT, const LinkedList& list);    
 };
 
 #endif
+
