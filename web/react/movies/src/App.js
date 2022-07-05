a587b0ff44c11728a49398064d4ae85ea0a8dde9
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-07-06 02:31:20 +0400

    Making some structural changes

--- web/react/movies/src/App.js
+++ web/react/movies/src/App.js
@@ -1,55 +1,15 @@
-import { useState, useEffect } from 'react';
-import Input from './components/Input';
-import Movies from './components/Movies';
-import Button from './components/Button';
-import AddMovieModal from './components/MovieModal';
-import movies from './movies';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import AppRouter from './components/AppRouter';
 
-function App() {
-	const [value, setValue] = useState('');
-	const [moviesList, setMoviesList] = useState(movies);
-	const [filteredMovies, setFilteredMovies] = useState(moviesList);
-	const [modal, setModal] = useState(false);
-
-	useEffect(() => {
-		setFilteredMovies(moviesList.filter((elem) => elem.name.toLowerCase().startsWith(value.toLowerCase())));
-	}, [value, moviesList]);
-	
-	useEffect(() => {
-		setFilteredMovies(moviesList);
-	}, [moviesList]);
-
-	const changeValue = (evt) => {
-		setValue(evt.target.value);
-	}
-
-	const showModal = () => {
-		setModal(true);
-	}
-
-	const closeModal = () => {
-		setModal(false);
-	}
-
-	const deleteMovie = (id) => {
-		setMoviesList(moviesList.filter((elem) => elem.id !== id));
-	}
-	
+function App() {	
 	return (
-		<div className='container'>
-			<div className='header'>
-				<p>Count: {filteredMovies.length}</p>
-				<Input
-					type="text"
-					placeholder="Search"
-					value={value}
-					onChange={changeValue}
-				/>
-				<Button type="button" buttonClick={showModal} buttonText="Add" />
-				{modal ? <AddMovieModal moviesList={moviesList} setMoviesList={setMoviesList} closeModal={closeModal}/> : null}
-			</div>
-			<Movies movies={filteredMovies} deleteMovie={deleteMovie} />
+		<div>
+			<BrowserRouter>
+				<Navbar />
+				<AppRouter />
+			</BrowserRouter>
 		</div>
 	);
 }
