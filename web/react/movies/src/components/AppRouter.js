379ed5499eb8950e54808ec165b625ed405df9c6
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-07-06 22:49:39 +0400

    Creating movie context for components

--- web/react/movies/src/components/AppRouter.js
+++ web/react/movies/src/components/AppRouter.js
@@ -1,17 +1,28 @@
+import React  from "react";
+import { useState, createContext } from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import MoviesPage from "./pages/movies/MoviesPage";
+import GoToMovie from "./pages/goToMovie/GoToMovie"
 import NotFoundPage from "./pages/notFound/NotFoundPage";
+import movies from '../movies';
+
+export const MovieContext = createContext(null);
 
 function AppRouter() {
-    return(
+    const [moviesList, setMoviesList] = useState(movies);
+    return (
+        <MovieContext.Provider value={{
+			moviesList, setMoviesList,
+		}}>
             <Routes> 
                 <Route path="/" />
                 <Route path="/home" element={<HomePage />} />
                 <Route path="/movies" element={<MoviesPage />} />
-                {/* <Route path="/movies/:id" /> */}
+                <Route path="/movies/:id" element={<GoToMovie />} />
                 <Route path="*" element={<NotFoundPage />} /> 
             </Routes>
+            </MovieContext.Provider>
     )
 }
 
