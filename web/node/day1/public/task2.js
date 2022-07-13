f7376371696bb15e668b1ffd9f16c55066181747
Ramelka <ramelkatamrazyan2001@gmail.com>
2022-07-14 01:32:27 +0400

    Changing the path of html file

--- web/node/day1/public/task2.js
+++ web/node/day1/public/task2.js
@@ -6,7 +6,7 @@ const port = 3000;
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile('../index.html', ((error, data) => {
+    fs.readFile('index.html', ((error, data) => {
         if (error) {
             response.writeHead(404);
             response.write('No such file');
